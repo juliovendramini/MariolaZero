2e867b854f05e3594ce471454b0253d51b786ef6
juliovendramini <juliovendramini@gmail.com>
2025-07-09 23:00:33 -0300

    limpeza

--- testeBanana/conds.py
+++
@@ -1,8 +0,0 @@
-import sensor as sen
-import crono as cr
-import constantes as consts
-
-def tudoBranco():
-    if cr.PretoDirEX.tempo() > 100 and cr.PretoDir.tempo() > 100 and cr.PretoEs.tempo() > 100 and cr.PretoEsEX.tempo() > 100:
-        return True
-    else: return False
